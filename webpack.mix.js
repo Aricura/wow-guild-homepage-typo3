9345347ecfc59f632663229624e7a41283cf7fab
Aricura <admin@aricura.com>
2018-07-23 23:25:05 +0200

    Enable asset versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,21 @@
 let mix = require('laravel-mix');
 
 mix
+	// set the website's root path relative to the project's root dir
+	.setPublicPath(path.resolve('./'))
+
 	// compile typescript
 	.ts('assets/main.ts', 'dist/')
 
 	// compile sass
 	.sass('assets/main.scss', 'dist/')
 
-	// set the website's root path relative to the project's root dir
-	.setPublicPath('/')
-
 	// customize the font output dir (created by sass compilation) relative to the root path
 	.options({
 		fileLoaderDirs: {
 			fonts: 'dist/fonts'
 		}
-	});
+	})
+
+	// using asset versioning for *.js and *.css files and fonts
+	.version();
